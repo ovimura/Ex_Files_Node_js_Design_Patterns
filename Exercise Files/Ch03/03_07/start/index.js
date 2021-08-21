72d51806cdbefc3b487743b88e262ade5911b5eb
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-20 19:41:58 -0700

    adding exercices for ch03, lesson 08

--- Exercise Files/Ch03/03_07/start/index.js
+++ Exercise Files/Ch03/03_07/start/index.js
@@ -1,10 +1,20 @@
 var Shopper = require('./Shopper');
-var InventoryItem = require('./InventoryItem');
+var { 
+    InventoryItem,
+    GoldenInventoryItem,
+    DiamondInventoryItem
+ } = require('./InventoryItem');
 
 var alex = new Shopper('Alex', 100);
 
 var walkman = new InventoryItem("Walkman", 29.99);
 var necklace = new InventoryItem("Necklace", 9.99);
+
+var gold_neclace = new GoldenInventoryItem(necklace);
+var diamond_gold_neclace = new DiamondInventoryItem(gold_neclace);
+
+var diamond_walkman = new DiamondInventoryItem(walkman);
+
 
 alex.purchase(necklace);
 alex.purchase(walkman);
