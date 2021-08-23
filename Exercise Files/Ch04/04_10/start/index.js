b091328d2115d8a2f967d35e87acfeb348c35e99
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2021-08-22 22:08:24 -0700

    adding exercices for ch04, lesson 11

--- Exercise Files/Ch04/04_10/start/index.js
+++ Exercise Files/Ch04/04_10/start/index.js
@@ -3,3 +3,9 @@ var logger = require('./Logger');
 logger.log('Hello World');
 logger.log('Hi World');
 logger.log('Yo World');
+
+logger.changeStrategy('toFile');
+
+logger.log('Hello World');
+logger.log('Hi World');
+logger.log('Yo World');
--- Exercise Files/Ch04/04_11/start/Logger.js
+++ Exercise Files/Ch04/04_11/start/Logger.js
@@ -1,19 +1,27 @@
+var LogStrategy = require('./LogStrategy');
+var config = require('./config.json');
+
 class Logger {
 
-    constructor() {
+    constructor(strategy='toConsole') {
         this.logs = [];
+        this.strategy = LogStrategy[strategy];
     }
 
     get count() {
         return this.logs.length
     }
 
+    changeStrategy(newStrategy) {
+        this.strategy = LogStrategy[newStrategy];
+    }
+
     log(message) {
         const timestamp = new Date().toISOString()
         this.logs.push({ message, timestamp })
-        console.log(`${timestamp} - ${message}`)
+        this.strategy(timestamp, message);
     }
 
 }
 
-module.exports = new Logger();
+module.exports = new Logger(config.logs.strategy);
